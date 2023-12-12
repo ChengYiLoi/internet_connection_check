5850d5e87f91224009719095426f967c2e02f094
Loi Cheng Yi <chengyi.loi.2019@smu.edu.sg>
2023-12-12 21:56:22 +0800

    added try except blocks

--- internet_connection_check.py
+++ internet_connection_check.py
@@ -6,13 +6,15 @@ import os
 def internet_connection_test(url:str):
   print(f'Attempting to connect to {url} to determine internet connection status.')
   
-  # ping uses ICMP protocol
-  # pinging with 5 packets via -c flag with 10 second timeout
-  response = os.popen(f"ping -c 5 -t 10 {url}")
-    
-  for line in response.readlines():
+  try:
+    # ping uses ICMP protocol
+  	# pinging with 5 packets via -c flag with 10 second timeout
+   response = os.popen(f"ping -c 5 -t 10 {url}")
+   for line in response.readlines():
       print(line)
-
+  except:
+    print(f"Failed with unparsed reason.")
+    
   # uses HTTP protocol
 	# try:
 	# 	print(url)
@@ -34,22 +36,25 @@ def bytes_to_mb(bytes:float):
   return float(bytes / pow(1024, 2))
 
 def check_download_speed(url:str):
-  print(f'Downloading file from {url} to determine download speed.')
-  start_time = time.time()
-  response = requests.get(url, stream=True)
-  
-  # in Bytes
-  total_length = float(response.headers.get('content-length'))
-
-  if total_length is not None:
-    # time since epoch in seconds
-    elapsed_time = time.time() - start_time
+  print(f'Attempting to downloade file from {url} to determine download speed.')
+  try:
+    start_time = time.time()
+    response = requests.get(url, stream=True)
     
-    print(f'Elapsed time: {elapsed_time}')
-    download_speed_mbps = bytes_to_mb(total_length) / elapsed_time
+    # in Bytes
+    total_length = float(response.headers.get('content-length'))
     
-    print(f'Total length in mb: {round(bytes_to_mb(total_length), 1)}')
-    print(f'Download speed in mbps: {round(download_speed_mbps, 1)}')
+    if total_length is not None:
+      # time since epoch in seconds
+      elapsed_time = time.time() - start_time
+      print(f'Elapsed time: {elapsed_time}')
+      
+      download_speed_mbps = bytes_to_mb(total_length) / elapsed_time
+      
+      print(f'Total length in mb: {round(bytes_to_mb(total_length), 1)}')
+      print(f'Download speed in mbps: {round(download_speed_mbps, 1)}')
+  except:
+    print(f"Failed with unparsed reason.")
 
 internet_connection_test(url='google.com')
 check_download_speed(url='https://drive.google.com/uc?export=download&id=1B6rLZwZx9QkzulhB0Pcgci4BXPlHEu3Z')
